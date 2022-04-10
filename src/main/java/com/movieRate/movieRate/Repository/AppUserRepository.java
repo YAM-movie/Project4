a72f047f9488a79e71ca59b7a15c033b1040215a
yousef Abu Qatrieh <yousef_1995q@hotmail.com>
2022-04-11 02:21:01 +0300

    yousefWork

--- src/main/java/com/movieRate/movieRate/Repository/AppUserRepository.java
+++ src/main/java/com/movieRate/movieRate/Repository/AppUserRepository.java
@@ -2,6 +2,8 @@ package com.movieRate.movieRate.Repository;
 
 import com.movieRate.movieRate.ModuleWeb.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser,Long> {
 }
