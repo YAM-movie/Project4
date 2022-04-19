eb043564e6f0b82e5e9390ede0ecd2b588b49a7d
A7mad3bd <ahmadabd398@gmail.com>
2022-04-19 19:15:02 +0300

    Add comment and roles

--- src/main/java/com/movieRate/movieRate/Services/Services.java
+++ src/main/java/com/movieRate/movieRate/Services/Services.java
@@ -3,8 +3,10 @@ package com.movieRate.movieRate.Services;
 import com.movieRate.movieRate.ModuleWeb.AppUser;
 import com.movieRate.movieRate.ModuleWeb.Movie;
 import com.movieRate.movieRate.ModuleWeb.Review;
+import com.movieRate.movieRate.ModuleWeb.Role;
 import org.springframework.security.core.Authentication;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -12,11 +14,14 @@ public interface Services {
     Movie getOneMovie(Long id, Model model);
     List<Movie> getAllMovies(Model model);
     Review getOneReview(Long id ,Model model);
-    List<Review> getAllReviewForOneMovie(Long MovieId , Model model);
+    //get All Reviews for one Movie by title of movie
+
+    List<Review> getAllReviewForOneMovie(String title, Model model);
     AppUser getUser(Long id,Model m);
     List<AppUser> getALLUsers(Model model);
     List<Movie> getAPi();
     void saveMovie(Movie movie ,Model model);
+
     AppUser getUserReviews(Long id, Model m);
     List<Movie> getPage(Long currentPage,Model m);
     Movie getMovieByTitle(String title, Model m);
@@ -32,5 +37,8 @@ public interface Services {
     void previousPage(Model model,long currentPage);
     boolean searchAboutMovie(String title,Model model);
     void trendingPage(Model model);
+    void addComment( String title, Authentication p, Model m, String body, int rate);
 
+    Role findRoleById(Long roleId);
+    Role findRoleByName(String name);
 }
